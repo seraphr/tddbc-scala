66d8b15061109464a7c8611385042f15998ed0b3
seraphr <seraph.sub+github@gmail.com>
2014-05-18 17:38:02 +0900

    TDDBC 最後

--- src/main/scala/jp/seraphr/tddbc/TodoCommand.scala
+++ src/main/scala/jp/seraphr/tddbc/TodoCommand.scala
@@ -1,29 +1,52 @@
 package jp.seraphr.tddbc
 
 import scala.collection.mutable.ArrayBuffer
+import scala.util.Try
 
 class TodoCommand {
-  
+
   val mTodos = new ArrayBuffer[Todo]
-  
+
   def add(aTodo: Todo): Unit = {
     mTodos += aTodo
   }
 
-  def getLastTodo(): Todo = {
-    mTodos.last
-  }
-  
-  def getFirstTodo(): Todo = {
-    mTodos.head
-  }
-  
+  def getLastTodo(): Option[Todo] =
+    if (mTodos.isEmpty)
+      None
+    else
+      Some(mTodos.last)
+
+  def getFirstTodo(): Option[Todo] =
+    if (mTodos.isEmpty)
+      None
+    else
+      Some(mTodos.head)
+
   def getTodos(): Seq[Todo] = {
     mTodos.toSeq
   }
+
+  def removeFirst(): Option[Todo] = {
+    if (mTodos.isEmpty)
+      None
+    else
+      Some(mTodos.remove(0))
+  }
+
+  def removeLast(): Option[Todo] = {
+    if (mTodos.isEmpty)
+      None
+    else
+      Some(mTodos.remove(mTodos.length - 1))
+  }
+  
+  def removeAll(): Unit = {
+    mTodos.clear()
+  }
 }
 
-object Todo{
+object Todo {
   def create(aTodoString: String): Todo = Todo(aTodoString)
 }
 
--- src/test/scala/jp/seraphr/tddbc/WhenRunningTodoCommand.scala
+++ src/test/scala/jp/seraphr/tddbc/WhenRunningTodoCommand.scala
@@ -3,12 +3,22 @@ package jp.seraphr.tddbc
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 import org.scalatest.BeforeAndAfter
+import scala.util.Success
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
-class WhenRunningTodoCommand extends FlatSpec with Matchers with BeforeAndAfter {
+class WhenRunningTodoCommand extends FlatSpec with Matchers with BeforeAndAfter with GeneratorDrivenPropertyChecks {
 
   private var mTodoCommand: TodoCommand = _
   before {
     mTodoCommand = new TodoCommand
+  }
+
+  private val firstTodo = Todo.create("first todo")
+  private val secondTodo = Todo.create("second todo")
+
+  private def addTwoTodos(): Unit = {
+    mTodoCommand.add(firstTodo)
+    mTodoCommand.add(secondTodo)
   }
 
   "user" should "TODOを追加できる" in {
@@ -22,18 +32,31 @@ class WhenRunningTodoCommand extends FlatSpec with Matchers with BeforeAndAfter 
     val tTodoCommand = mTodoCommand
     import tTodoCommand._
 
-    add(Todo.create("command1"))
-    add(Todo.create("command2"))
-    getLastTodo() should equal(Todo.create("command2"))
+    addTwoTodos()
+    getLastTodo() should equal(Some(secondTodo))
+  }
+
+  "user" should "0件のときは最後のTODOを取得できない" in {
+    val tTodoCommand = mTodoCommand
+    import tTodoCommand._
+
+    getLastTodo() should be(None);
   }
 
   "user" should "最初に追加したTODOを取得できる" in {
     val tTodoCommand = mTodoCommand
     import tTodoCommand._
 
-    add(Todo.create("command1"))
-    add(Todo.create("command2"))
-    getFirstTodo() should equal(Todo.create("command1"))
+    addTwoTodos()
+
+    getFirstTodo() should equal(Some(firstTodo))
+  }
+
+  "user" should "0件のときは最初のTODOを取得できない" in {
+    val tTodoCommand = mTodoCommand
+    import tTodoCommand._
+
+    getFirstTodo() should be(None);
   }
 
   "user" should "追加した全てのTODOを取得できる" in {
@@ -48,5 +71,54 @@ class WhenRunningTodoCommand extends FlatSpec with Matchers with BeforeAndAfter 
       Todo.create("最初に追加した文字列"),
       Todo.create("2番目"),
       Todo.create("最後に追加した文字列")))
+  }
+
+  "user" should "TODOを2個を追加したあと、最初のTODOを削除すると、2つ目に追加したものだけになる" in {
+    val tTodoCommand = mTodoCommand
+    import tTodoCommand._
+
+    addTwoTodos()
+
+    removeFirst()
+    getTodos() should be(Seq(secondTodo))
+  }
+
+  "user" should "TODOが空の状態で最初のTODOを削除しても、空のままになる" in {
+    val tTodoCommand = mTodoCommand
+    import tTodoCommand._
+
+    getTodos().isEmpty should be(true)
+    removeFirst()
+    getTodos().isEmpty should be(true)
+  }
+
+  "user" should "TODOを2個を追加したあと、最後のTODOを削除すると、1つ目に追加したものだけになる" in {
+    val tTodoCommand = mTodoCommand
+    import tTodoCommand._
+
+    addTwoTodos()
+
+    removeLast()
+    getTodos() should be(Seq(firstTodo))
+  }
+
+  "user" should "TODOが空の状態で最後のTODOを削除しても、空のままになる" in {
+    val tTodoCommand = mTodoCommand
+    import tTodoCommand._
+
+    getTodos().isEmpty should be(true)
+    removeLast()
+    getTodos().isEmpty should be(true)
+  }
+
+  "user" should "TODOを何個か追加したあと、全てを削除すると、空になる" in {
+    val tTodoCommand = mTodoCommand
+    import tTodoCommand._
+
+    forAll { tTodos: List[String] =>
+      tTodos.map(Todo.create).foreach(tTodoCommand.add)
+      tTodoCommand.removeAll()
+      tTodoCommand.getTodos() should have size 0
+    }
   }
 }
